a21f2426d6be5c7c26a488ead07a21bad75bd643
Ali-Dovrushov <dovrushov7@gmail.com>
2021-04-28 07:36:11 +0400

    Recursion(Power)

--- Question3/Question3/Program.cs
+++ Question3/Question3/Program.cs
@@ -4,9 +4,96 @@ namespace Question3
 {
     class Program
     {
+
+        static int Pow(int num, int pow)
+        {
+            if (pow == 0)
+            {
+                return 1;
+            }
+
+            int number = num * Pow(num, pow - 1);
+
+            return number;
+        }
+
+        static int NumberChecker()
+        {
+            int number;
+
+            for (; ; )
+            {
+                string numberInString = Console.ReadLine();
+
+                if (Int32.TryParse(numberInString, out number))
+                {
+                    return number;
+                }
+
+                else
+                {
+                    Console.Write("Incorrect number, please enter again: ");
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool selection = true;
+            bool selectionForSwitch = false;
+            do
+            {
+                Console.Write("Enter number: ");
+                int number = NumberChecker();
+                Console.Write("Enter power: ");
+                int pow = NumberChecker();
+                Console.WriteLine($"Result: {Pow(number, pow)}");
+
+                Console.Write("\nWould you try again ? (Y/y) or (N/n): ");
+                do
+                {
+                    string symbolForSwitch = Convert.ToString(Console.ReadLine());
+                    switch (symbolForSwitch)
+                    {
+                        case "Y":
+                            {
+                                selection = true;
+                                selectionForSwitch = true;
+                                break;
+                            }
+                        case "y":
+                            {
+                                selection = true;
+                                selectionForSwitch = true;
+                                break;
+                            }
+                        case "N":
+                            {
+                                Console.WriteLine("Have a good day :)");
+                                selection = false;
+                                selectionForSwitch = true;
+                                break;
+                            }
+                        case "n":
+                            {
+                                Console.WriteLine("Have a good day :)");
+                                selection = false;
+                                selectionForSwitch = true;
+                                break;
+                            }
+                        default:
+                            {
+                                Console.Write("You enter incorrect symbol, please enter Y/y or N/n: ");
+                                selectionForSwitch = false;
+                                selection = true;
+                                continue;
+                            }
+                    }
+                } while (selectionForSwitch == false);
+            }
+            while (selection == true);
+            Console.ReadKey();
+
         }
     }
 }
